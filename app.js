e539b1f0a6ed1a6eb925a694b3087cf9a97f87db
Vivekko <vivekojha961@gmail.com>
2021-05-17 23:25:58 +0530

    Update

--- app.js
+++ app.js
@@ -3,24 +3,32 @@ const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
 const Reset = document.querySelector('#reset');
+const ws = document.querySelector('#DD');
 
 let p1Score = 0,p2Score = 0 ;
-let win = 5;
+let win = ws;
 
 p1Button.addEventListener('click',function(){
     if(p1Score != win && p2Score != win){
         p1Score += 1;
         p1Display.textContent = p1Score;
-    }
+    }else
+    ResetFx();
 });
 p2Button.addEventListener('click',function(){
     if(p1Score != win && p2Score != win){
         p2Score += 1;
         p2Display.textContent = p2Score;
     }
+    else
+    ResetFx();
 });
-Reset.addEventListener('click',function(){
+function ResetFx(){
     p1Score = 0;p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
+}
+Reset.addEventListener('click',ResetFx);
+ws.addEventListener('change',function(){
+    win = parseInt(this.value);
 })
