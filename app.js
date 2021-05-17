97fc197aaa662cd39b972889bd7fda96a11962db
Vivekko <vivekojha961@gmail.com>
2021-05-17 22:56:47 +0530

    Added Raw HTML and Js

---
+++ app.js
@@ -0,0 +1,26 @@
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const Reset = document.querySelector('#reset');
+
+let p1Score = 0,p2Score = 0 ;
+let win = 5;
+
+p1Button.addEventListener('click',function(){
+    if(p1Score != win && p2Score != win){
+        p1Score += 1;
+        p1Display.textContent = p1Score;
+    }
+});
+p2Button.addEventListener('click',function(){
+    if(p1Score != win && p2Score != win){
+        p2Score += 1;
+        p2Display.textContent = p2Score;
+    }
+});
+Reset.addEventListener('click',function(){
+    p1Score = 0;p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+})
